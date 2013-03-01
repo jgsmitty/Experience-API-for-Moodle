b170c1a4cd4d13bd4cec5a1565ea074258aea2a1
Jamie Smith <jamie.g.smith@gmail.com>
2013-02-28 20:03:07 -0500

    Content end point logic change
    A modification was made to the content enpoint of the API to eliminate
    the 301 redirect method. It is presumed that it will be more efficient
    as well as solve a discovered issue preventing the Articulate Mobile
    Player from being able to access an embedded video when requested from
    the content end point.

--- content_endpoint.php
+++ content_endpoint.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * The content end point expects a relative file path to be passed just as Moodle's file.php.
+ * The purpose in using this method vs. a redirect is to eliminate a round-trip request.
+ * This proves to be effective in delivering all media securely when requested by the Articulate Mobile Player.
+ */
+/**
+ * AJAX_SCRIPT - exception will be converted into JSON
+ */
+define('AJAX_SCRIPT', true);
 
 /**
  * NO_DEBUG_DISPLAY - disable moodle specific debug messages and any errors in output
@@ -10,8 +19,8 @@ define('NO_DEBUG_DISPLAY', true);
  */
 define('NO_MOODLE_COOKIES', true);
 
-include_once '../../config.php';
-include_once './locallib.php';
+require_once '../../config.php';
+require_once './locallib.php';
 
 ob_start();
 
@@ -20,39 +29,57 @@ echo "\n\nNew submit:\n";
 $parameters = array(); 
 if (isset($_SERVER['QUERY_STRING']))
 	parse_str($_SERVER['QUERY_STRING'], $parameters);
-if (isset($_SERVER['PATH_INFO'])) {
-	echo $_SERVER['PATH_INFO']."\n";
-	$url = $CFG->wwwroot.$_SERVER['PATH_INFO'];
-}
-//echo 'Parameters: '."\n";
-//print_r($parameters);
+echo 'Parameters: '."\n";
+print_r($parameters);
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET'
-	&& isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] == 'app:/Articulate.swf'
 	&& isset($parameters['content_token'])) {
-		$url .= '?token='.$parameters['content_token'];
-		$action = 'redirect';
+		$token = $parameters['content_token'];
+		$action = 'return_file';
 	} else {
+		print_r($parameters);
+		print_r($_SERVER);
 		$action = 'deny';
 	} 
 
-//echo "\n";
-if (isset($url))
-	echo 'URL = '.$url."\n";
 echo 'Action = '.$action."\n";
 	    
-$contents = ob_get_contents();
-ob_end_clean();
-if (TCAPI_LOG_CONTENT_ENDPOINT) {
-	$h = fopen("content_log.txt",'a+');
-	fwrite($h, $contents);
-	fclose($h);
-}
-
-if ($action == 'redirect')
-	header('Location: '.$url,TRUE,301);
-else
-	header('HTTP/1.0 401 Unauthorized',TRUE,401);			
+if ($action == 'return_file') {
+	/*
+	 * Use the same method as webservice/pluginfile.php to return file directly.
+	 */
+	require_once($CFG->libdir . '/filelib.php');
+	require_once($CFG->dirroot . '/webservice/lib.php');
 	
-exit;       
-?>
+	//authenticate the user
+	$webservicelib = new webservice();
+	$authenticationinfo = $webservicelib->authenticate_user($token);
+	
+	//check the service allows file download
+	$enabledfiledownload = (int) ($authenticationinfo['service']->downloadfiles);
+	if (empty($enabledfiledownload)) {
+		throw new webservice_access_exception('Web service file downloading must be enabled in external service settings');
+	}
+	
+	//finally we can serve the file :)
+	$relativepath = get_file_argument();
+	echo 'Relative path = '.$relativepath."\n";
+	tcapi_create_content_log();
+	
+	file_pluginfile($relativepath, 0);
+}
+else {
+	tcapi_create_content_log();
+	header('HTTP/1.0 401 Unauthorized',TRUE,401);			
+}
+exit;
+
+function tcapi_create_content_log() {
+	$contents = ob_get_contents();
+	ob_end_clean();
+	if (TCAPI_LOG_CONTENT_ENDPOINT) {
+		$h = fopen("content_log.txt",'a+');
+		fwrite($h, $contents);
+		fclose($h);
+	}
+}
